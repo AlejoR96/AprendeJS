8429b77025f20b962b991ba42e5dea5a3656854f
Alejandro Rivera <jalejandro142@gmail.com>
2023-07-15 00:09:22 -0500

    se agrega img y anotacion sobre typeof

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ let welcomeText = "Hola, Bienvenido";
 let isCool = true;
 
 console.log("Esto es un mensaje para la consola");
+
+// typeOf
+
+// Usando con operadores de comparacion.
+
+const age = 42
+typeof age === "number"
